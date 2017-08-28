88944e29ecea8ec0add6de218596cd3e27356fd2
Remi Rampin <remirampin@gmail.com>
2017-08-28 17:05:27 -0400

    Update code for proto

--- client.py
+++ client.py
@@ -3,14 +3,47 @@ import d3m_ta2_vistrails.proto.pipeline_service_pb2 as ps_pb2
 import d3m_ta2_vistrails.proto.pipeline_service_pb2_grpc as ps_pb2_grpc
 
 
+__version__ = '0.1'
+
+
 if __name__ == '__main__':
     channel = grpc.insecure_channel('localhost:50051')
     stub = ps_pb2_grpc.PipelineComputeStub(channel)
 
-    reply = stub.StartSession(ps_pb2.SessionRequest())
+    version = ps_pb2.DESCRIPTOR.GetOptions().Extensions[
+        ps_pb2.protocol_version]
+
+    reply = stub.StartSession(ps_pb2.SessionRequest(
+        user_agent='text_client %s' % __version__,
+        version=version,
+    ))
     context = reply.context
     print "Started session %r, status %s" % (context.session_id,
-                                             reply.status.code)
+                                             reply.response_info.status.code)
+
+    reply = stub.CreatePipelines(ps_pb2.PipelineCreateRequest(
+        context=context,
+        train_features=[
+            ps_pb2.Feature(feature_id='feature1',
+                           data_uri='file:///data/feature1.csv'),
+            ps_pb2.Feature(feature_id='feature2',
+                           data_uri='file:///data/feature2.csv'),
+        ],
+        task=ps_pb2.CLASSIFICATION,
+        task_subtype=ps_pb2.NONE,
+        task_description="Debugging task",
+        output=ps_pb2.PROBABILITY,
+        metrics=[
+            ps_pb2.ACCURACY,
+            ps_pb2.ROC_AUC,
+        ],
+        target_features=[
+            ps_pb2.Feature(feature_id='targetfeature',
+                           data_uri='file:///data/targetfeature.csv'),
+        ],
+        max_pipelines=10,
+    ))
+    print("Requested pipelines")
 
     reply = stub.EndSession(context)
     print "Ended session %r, status %s" % (context.session_id,
--- d3m_ta2_vistrails/main.py
+++ d3m_ta2_vistrails/main.py
@@ -3,6 +3,7 @@ import grpc
 import logging
 import time
 
+from d3m_ta2_vistrails import __version__
 import d3m_ta2_vistrails.proto.pipeline_service_pb2 as ps_pb2
 import d3m_ta2_vistrails.proto.pipeline_service_pb2_grpc as ps_pb2_grpc
 
@@ -12,29 +13,35 @@ logger = logging.getLogger(__name__)
 
 class ComputeService(ps_pb2_grpc.PipelineComputeServicer):
     def StartSession(self, request, context):
-        logger.info("Session started: 1")
-        return ps_pb2.Response(
+        version = ps_pb2.DESCRIPTOR.GetOptions().Extensions[
+            ps_pb2.protocol_version]
+        logger.info("Session started: 1 (protocol version %s)", version)
+        return ps_pb2.SessionResponse(
+            response_info=ps_pb2.Response(
+                status=ps_pb2.Status(code=ps_pb2.OK)
+            ),
+            user_agent='vistrails_ta2 %s' % __version__,
+            version=version,
             context=ps_pb2.SessionContext(session_id='1'),
-            status=ps_pb2.Status(code=ps_pb2.OK),
         )
 
     def EndSession(self, request, context):
         assert request.session_id == '1'
         logger.info("Session terminated: 1")
         return ps_pb2.Response(
-            context=ps_pb2.SessionContext(session_id=request.session_id),
             status=ps_pb2.Status(code=ps_pb2.OK),
         )
 
     def CreatePipelines(self, request, context):
         sessioncontext = request.context
         assert sessioncontext.session_id == '1'
-        dataset_uris = request.train_dataset_uris
+        train_features = request.train_features
         task = request.task
-        assert task == ps_pb2.TaskType.CLASSIFICATION
+        assert task == ps_pb2.CLASSIFICATION
+        task_subtype = request.task_subtype
         task_description = request.task_description
         output = request.output
-        metrics = request.metric
+        metrics = request.metrics
         target_features = request.target_features
         max_pipelines = request.max_pipelines
 
@@ -44,18 +51,23 @@ class ComputeService(ps_pb2_grpc.PipelineComputeServicer):
         while True:
             yield ps_pb2.PipelineCreateResult(
                 response_info=ps_pb2.Response(
-                    context=sessioncontext,
                     status=ps_pb2.Status(code=ps_pb2.OK),
                 ),
-                progress_info=ps_pb2.Progress.SUBMITTED,
+                progress_info=ps_pb2.COMPLETED,
                 pipeline_id="pipeline_1",
                 pipeline_info=ps_pb2.Pipeline(
-                    predict_result_uris=[],
+                    predict_result_uris=['file:///out/predict1.csv'],
                     output=output,
-                    score=ps_pb2.Score(
-                        metric=ps_pb2.Metric.ACCURACY,
-                        value=1.0,
-                    ),
+                    scores=[
+                        ps_pb2.Score(
+                            metric=ps_pb2.ACCURACY,
+                            value=0.8,
+                        ),
+                        ps_pb2.Score(
+                            metric=ps_pb2.ROC_AUC,
+                            value=0.5,
+                        ),
+                    ],
                 ),
             )
 
