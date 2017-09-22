74eb3f18de33404490f93cb0560f974a1e27a18f
Yamuna Krishnamurthy <yamuna@nyu.edu>
2017-09-22 17:47:53 -0400

    Added ReadDataSchema to read the D3M datasSchema.json and load trainData, trainTargets and testData

--- userpackages/d3m/init.py
+++ userpackages/d3m/init.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 from vistrails.core.modules.utils import make_modules_dict
 from .primitives import _modules as primitives_modules
+from .read import _modules as read_modules
 
-_modules = [primitives_modules]
+_modules = [primitives_modules, read_modules]
 _modules = make_modules_dict(*_modules)
