d2f4a1d89e8df74771b433e6dcce3289eece6150
Remi Rampin <remirampin@gmail.com>
2017-09-27 12:30:15 -0400

    Scripts: one script pushes to both locations

--- scripts/push_to_hub.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker images | grep ta2/nyu_ta2 | awk '{ print $2; }' | while read i; do if [ "$i" != "<none>" ]; then docker tag registry.datadrivendiscovery.org/ta2/nyu_ta2:$i remram/d3m_nyu_ta2_vistrails:$i; docker push remram/d3m_nyu_ta2_vistrails:$i; fi; done
