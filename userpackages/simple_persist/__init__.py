79e68c08737d40c2481fb602a1d0878b5bcf81e7
Remi Rampin <remirampin@gmail.com>
2017-09-27 11:14:49 -0400

    Add simple_persist package to serialize primitives

---
+++ userpackages/simple_persist/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import division
+
+from vistrails.core.configuration import ConfigurationObject
+
+
+identifier = 'org.vistrails.vistrails.persist'
+name = 'simple_persist'
+version = '0.0.1'
+
+
+configuration = ConfigurationObject(file_store=(None, str))
---
+++ userpackages/simple_persist/init.py
@@ -0,0 +1,38 @@
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+import os.path
+from vistrails.core.modules.vistrails_module import Module, ModuleError
+
+from . import configuration
+
+
+class Persist(Module):
+    _input_ports = [('value', '(org.vistrails.vistrails.basic:Variant)')]
+    _output_ports = [('value', '(org.vistrails.vistrails.basic:Variant)')]
+
+    def compute(self):
+        module_id = self.moduleInfo['moduleId']
+
+        if self.has_input('value'):
+            value = self.get_input('value')
+            self.set_output('value', value)
+
+            if configuration.has('file_store'):
+                path = os.path.join(configuration.file_store,
+                                    '%d.pkl' % module_id)
+                with open(path, 'wb') as fp:
+                    pickle.dump(value, fp)
+        else:
+            if not configuration.has('file_store'):
+                raise ModuleError(self, "No input and no path to load from")
+
+            path = os.path.join(configuration.file_store,
+                                '%d.pkl' % module_id)
+            with open(path, 'rb') as fp:
+                value = pickle.load(fp)
+            self.set_output('value', value)
+
+
+_modules = [Persist]
