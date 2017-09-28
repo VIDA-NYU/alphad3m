0f7e48f5c95bf8c4cf91b91de8966515fb2e7f54
Remi Rampin <remirampin@gmail.com>
2017-09-28 15:50:37 -0400

    Remove leftover method from previous commit

--- userpackages/d3m/read/readdataset.py
+++ userpackages/d3m/read/readdataset.py
@@ -25,37 +25,6 @@ class ReadDataSchema(Module):
         ('testData_list',  '(org.vistrails.vistrails.basic:List)'), ('testData_frame', '(org.vistrails.vistrails.basic:List)'),
     ]
 
-    def _readFile(self, data_schema, filename, data_type, output_type,
-                  single_column=False):
-        try:
-            data_frame = pd.read_csv(filename)
-            data_column_names = []
-            data_index = []
-            for feature in data_schema[data_type]:
-                if 'index' in feature['varRole']:
-                    data_index = list(data_frame[feature['varName']])
-                    self.set_output(output_type + '_index', data_index)
-                elif 'attribute' in feature['varRole']:
-                    data_column_names.append(feature['varName'])
-                elif 'target' in feature['varRole']:
-                    data_column_names.append(feature['varName'])
-
-            data_columns = data_frame.keys()
-            for key in data_columns:
-                if key not in data_column_names:
-                    data_frame = data_frame.drop(key, axis=1)
-
-            data_frame = pd.DataFrame(data=data_frame, index=data_index)
-            self.set_output(output_type + '_columns', data_column_names)
-            list_ = data_frame.as_matrix()
-            if single_column:
-                assert list_.shape[1] == 1
-                list_ = list_.reshape((-1,))
-            self.set_output(output_type + '_list', list_)
-            self.set_output(output_type + '_frame', data_frame)
-        except IOError:
-            print output_type + " not found"
-
     def compute(self):
         data = read_dataset(self.get_input('data_path').name)
         for output_type in ['trainData', 'trainTargets', 'testData']:
