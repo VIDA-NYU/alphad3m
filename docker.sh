8c1d8165d8c2e5578f8ba081985b7c77c790310d
Remi Rampin <remirampin@gmail.com>
2018-01-16 14:55:52 -0500

    Update example configuration files

---
+++ docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Example train: ./docker.sh ta2-test:latest ta2_search /d3m/config_train.json
+# Example test: ./docker.sh ta2-test:latest /d3m/tmp/executables/50f99cdf-97f7-45d7-8095-35778c39093f /d3m/config_test.json
+
+docker run -ti --rm -v /Users/remram/Documents/programming/d3m/data:/d3m/data -v /Users/remram/Documents/programming/d3m/tmp:/d3m/tmp -v "$PWD/config_train.json:/d3m/config_train.json" -v "$PWD/config_test.json:/d3m/config_test.json" "$@"
