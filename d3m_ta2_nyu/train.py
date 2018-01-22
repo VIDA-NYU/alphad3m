c62e1b54e7e045d8a15b2a1fcf3cfff7fef5cc2d
Remi Rampin <remirampin@gmail.com>
2018-01-22 16:08:32 -0500

    Split cross_validation() into its own function

--- d3m_ta2_nyu/train.py
+++ d3m_ta2_nyu/train.py
@@ -16,6 +16,67 @@ FOLDS = 3
 SPLIT_RATIO = 0.25
 
 
+def cross_validation(pipeline, metrics, data, targets, progress, db):
+    scores = {}
+
+    for i in range(FOLDS):
+        logger.info("Scoring round %d/%d", i + 1, FOLDS)
+
+        progress(i)
+
+        # Do the split
+        random_sample = numpy.random.rand(len(data)) < SPLIT_RATIO
+
+        train_data_split = data[random_sample]
+        test_data_split = data[~random_sample]
+
+        train_target_split = targets[random_sample]
+        test_target_split = targets[~random_sample]
+
+        start_time = time.time()
+
+        # Run training
+        logger.info("Training on fold")
+        try:
+            train_run, outputs = execute_train(
+                db, pipeline, train_data_split, train_target_split,
+                crossval=True)
+        except Exception:
+            logger.exception("Error running training on fold")
+            sys.exit(1)
+        assert train_run is not None
+
+        # Run prediction
+        logger.info("Testing on fold")
+        try:
+            test_run, outputs = execute_test(
+                db, pipeline, test_data_split,
+                crossval=True, from_training_run_id=train_run)
+        except Exception:
+            logger.exception("Error running testing on fold")
+            sys.exit(1)
+
+        run_time = time.time() - start_time
+
+        predictions = next(iter(outputs.values()))['predictions']
+
+        # Compute score
+        for metric in metrics:
+            # Special case
+            if metric == 'EXECUTION_TIME':
+                scores.setdefault(metric, []).append(run_time)
+            else:
+                score_func = SCORES_TO_SKLEARN[metric]
+                scores.setdefault(metric, []).append(
+                    score_func(test_target_split, predictions))
+
+    progress(FOLDS)
+
+    # Aggregate results over the folds
+    return {metric: numpy.mean(values)
+            for metric, values in scores.items()}
+
+
 @database.with_db
 def train(pipeline_id, metrics, dataset, problem, msg_queue, db):
     logging.basicConfig(
@@ -39,62 +100,11 @@ def train(pipeline_id, metrics, dataset, problem, msg_queue, db):
     # Scoring step - make folds, run them through the pipeline one by one
     # (set both training_data and test_data),
     # get predictions from OutputPort to get cross validation scores
-    scores = {}
-
-    for i in range(FOLDS):
-        logger.info("Scoring round %d/%d", i + 1, FOLDS)
-
-        msg_queue.put((pipeline_id, 'progress', (i + 1.0) / max_progress))
-
-        # Do the split
-        random_sample = numpy.random.rand(len(data)) < SPLIT_RATIO
-
-        train_data_split = data[random_sample]
-        test_data_split = data[~random_sample]
-
-        train_target_split = targets[random_sample]
-        test_target_split = targets[~random_sample]
-
-        start_time = time.time()
-
-        # Run training
-        logger.info("Training on fold")
-        try:
-            train_run, outputs = execute_train(
-                db, pipeline_id, train_data_split, train_target_split,
-                crossval=True)
-        except Exception:
-            logger.exception("Error running training on fold")
-            sys.exit(1)
-        assert train_run is not None
-
-        # Run prediction
-        logger.info("Testing on fold")
-        try:
-            test_run, outputs = execute_test(
-                db, pipeline_id, test_data_split,
-                crossval=True, from_training_run_id=train_run)
-        except Exception:
-            logger.exception("Error running testing on fold")
-            sys.exit(1)
-
-        run_time = time.time() - start_time
-
-        predictions = next(iter(outputs.values()))['predictions']
-
-        # Compute score
-        for metric in metrics:
-            # Special case
-            if metric == 'EXECUTION_TIME':
-                scores.setdefault(metric, []).append(run_time)
-            else:
-                score_func = SCORES_TO_SKLEARN[metric]
-                scores.setdefault(metric, []).append(
-                    score_func(test_target_split, predictions))
-
-    msg_queue.put((pipeline_id, 'progress', (FOLDS + 1.0) / max_progress))
-
-    # Aggregate results over the folds
+    scores = cross_validation(
+        pipeline_id, metrics, data, targets,
+        lambda i: msg_queue.put((pipeline_id, 'progress',
+                                 (i + 1.0) / max_progress)),
+        db)
     scores = [database.CrossValidationScore(metric=metric,
                                             value=numpy.mean(values))
               for metric, values in scores.items()]
