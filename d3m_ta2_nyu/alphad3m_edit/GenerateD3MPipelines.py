9d644d04c224db6c9c285a3207d080017c69cb15
Yamuna Krishnamurthy <yamunak@gmail.com>
2018-07-26 16:57:57 -0400

    Added pipeline for regression on image data

--- d3m_ta2_nyu/alphad3m_edit/GenerateD3MPipelines.py
+++ d3m_ta2_nyu/alphad3m_edit/GenerateD3MPipelines.py
@@ -239,7 +239,7 @@ class GenerateD3MPipelines():
         def make_primitive_module(name):
             if name[0] == '.':
                 name = 'd3m.primitives' + name
-            return make_module('d3m', '2018.6.5', name)
+            return make_module('d3m', '2018.7.10', name)
 
         def connect(from_module, to_module,
                     from_output='produce', to_input='inputs'):
@@ -291,7 +291,7 @@ class GenerateD3MPipelines():
         def make_primitive_module(name):
             if name[0] == '.':
                 name = 'd3m.primitives' + name
-            return make_module('d3m', '2018.6.5', name)
+            return make_module('d3m', '2018.7.10', name)
 
         def connect(from_module, to_module,
                     from_output='produce', to_input='inputs'):
@@ -323,3 +323,85 @@ class GenerateD3MPipelines():
             return pipeline.id
         finally:
             db.close()
+
+    @staticmethod
+    def make_image_regression_pipeline_from_strings(origin, dataset, targets=None, features=None,
+                                                    DBSession=None):
+        logger.info('MAKING IMAGE REGRESSION PIPELINE')
+        db = DBSession()
+
+        pipeline = database.Pipeline(
+            origin=origin,
+            dataset=dataset)
+
+        def make_module(package, version, name):
+            pipeline_module = database.PipelineModule(
+                pipeline=pipeline,
+                package=package, version=version, name=name)
+            db.add(pipeline_module)
+            return pipeline_module
+
+        def make_data_module(name):
+            return make_module('data', '0.0', name)
+
+        def make_primitive_module(name):
+            if name[0] == '.':
+                name = 'd3m.primitives' + name
+            return make_module('d3m', '2018.7.10', name)
+
+        def connect(from_module, to_module,
+                    from_output='produce', to_input='inputs'):
+            db.add(database.PipelineConnection(pipeline=pipeline,
+                                               from_module=from_module,
+                                               to_module=to_module,
+                                               from_output_name=from_output,
+                                               to_input_name=to_input))
+
+        try:
+
+            input_data = make_data_module('dataset')
+            db.add(database.PipelineParameter(
+                pipeline=pipeline, module=input_data,
+                name='targets', value=pickle.dumps(targets),
+            ))
+            db.add(database.PipelineParameter(
+                pipeline=pipeline, module=input_data,
+                name='features', value=pickle.dumps(features),
+            ))
+            primitives = ["d3m.primitives.datasets.DatasetToDataFrame", "d3m.primitives.data.ExtractColumnsBySemanticTypes",
+                          "d3m.primitives.dsbox.DataFrameToTensor", "d3m.primitives.dsbox.Vgg16ImageFeature",
+                          "d3m.primitives.sklearn_wrap.SKPCA", "d3m.primitives.sklearn_wrap.SKRandomForestRegressor"]
+            step0 = make_primitive_module("d3m.primitives.dsbox.Denormalize")
+            connect(input_data, step0, from_output='dataset')
+
+            step = prev_step = step0
+            preprocessors = []
+            if len(primitives) > 1:
+                preprocessors = primitives[0:len(primitives) - 1]
+            classifier = primitives[len(primitives) - 1]
+            for preprocessor in preprocessors:
+                step = make_primitive_module(preprocessor)
+                connect(prev_step, step)
+                prev_step = step
+
+            # step1 = make_primitive_module('.datasets.DatasetToDataFrame')
+            # connect(input_data, step1, from_output='dataset')
+            #
+            # step2 = make_primitive_module('.data.ExtractTargets')
+            # connect(step1, step2)
+            #
+            # step3 = make_primitive_module('.data.CastToType')
+            # connect(step2, step3)
+
+            step4 = make_primitive_module(classifier)
+            connect(step, step4)
+            # connect(step3, step4, to_input='outputs')
+
+            db.add(pipeline)
+            db.commit()
+            logger.info('%s PIPELINE ID: %s', origin, pipeline.id)
+            return pipeline.id
+
+        finally:
+            db.close()
+
