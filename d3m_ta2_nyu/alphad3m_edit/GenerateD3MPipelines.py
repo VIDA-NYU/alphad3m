f41d9df652a90421689e79d89fbde8bac68b524a
Roque Lopez <rlopezc27@gmail.com>
2019-05-15 18:31:20 -0400

    Update primitive names

--- d3m_ta2_nyu/alphad3m_edit/GenerateD3MPipelines.py
+++ d3m_ta2_nyu/alphad3m_edit/GenerateD3MPipelines.py
@@ -411,7 +411,7 @@ class GenerateD3MPipelines():
             step2 = make_primitive_module('d3m.primitives.data_transformation.column_parser.DataFrameCommon')
             connect(step1, step2)
 
-            step3 = make_primitive_module('d3m.primitives.sri.psl.GraphMatchingLinkPrediction')
+            step3 = make_primitive_module('d3m.primitives.link_prediction.graph_matching_link_prediction.GraphMatchingLinkPrediction')
             set_hyperparams(step3, link_prediction_hyperparams="gANjc3JpLnBzbC5saW5rX3ByZWRpY3Rpb24KTGlua1ByZWRpY3Rpb25IeXBlcnBhcmFtcwpxACmBcQF9cQIoWAsAAABwc2xfb3B0aW9uc3EDWAAAAABxBFgMAAAAcHNsX3RlbXBfZGlycQVYDAAAAC90bXAvcHNsL3J1bnEGWBAAAABwb3N0Z3Jlc19kYl9uYW1lcQdYBwAAAHBzbF9kM21xCFgPAAAAYWRtbV9pdGVyYXRpb25zcQlN6ANYCwAAAG1heF90aHJlYWRzcQpLAFgKAAAAanZtX21lbW9yeXELRz/oAAAAAAAAWA8AAAB0cnV0aF90aHJlc2hvbGRxDEc+etfymryvSFgRAAAAcHJlZGljdGlvbl9jb2x1bW5xDVgEAAAAbGlua3EOdWIu")
 
             connect(input_data, step3, from_output='dataset')
@@ -485,9 +485,9 @@ class GenerateD3MPipelines():
                 pipeline=pipeline, module=input_data,
                 name='features', value=pickle.dumps(features),
             ))
-            step0 = make_primitive_module('d3m.primitives.sri.graph.CommunityDetectionParser')
+            step0 = make_primitive_module('d3m.primitives.community_detection.community_detection_parser.CommunityDetectionParser')
             connect(input_data, step0, from_output='dataset')
-            step1 = make_primitive_module('d3m.primitives.sri.psl.CommunityDetection')
+            step1 = make_primitive_module('d3m.primitives.classification.community_detection.CommunityDetection')
             set_hyperparams(
                 step1,
                 jvm_memory=0.8
@@ -544,11 +544,11 @@ class GenerateD3MPipelines():
                 pipeline=pipeline, module=input_data,
                 name='features', value=pickle.dumps(features),
             ))
-            step0 = make_primitive_module('d3m.primitives.sri.graph.GraphMatchingParser')
+            step0 = make_primitive_module('d3m.primitives.data_transformation.graph_matching_parser.GraphMatchingParser')
             connect(input_data, step0, from_output='dataset')
-            step1 = make_primitive_module('d3m.primitives.sri.graph.GraphTransformer')
+            step1 = make_primitive_module('d3m.primitives.data_transformation.graph_transformer.GraphTransformer')
             connect(step0, step1)
-            step2 = make_primitive_module('d3m.primitives.sri.psl.LinkPrediction')
+            step2 = make_primitive_module('d3m.primitives.link_prediction.link_prediction.LinkPrediction')
             connect(step1, step2)
             step3 = make_primitive_module('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon')
             connect(step2, step3)
@@ -675,7 +675,7 @@ class GenerateD3MPipelines():
             connect(step1, step2)
 
             step3 = make_primitive_module('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon')
-            set_hyperparams(step3, semantic_types=['https://metadata.datadrivendiscovery.org/types/ImageObject'])
+            set_hyperparams(step3, semantic_types=['http://schema.org/ImageObject'])
             connect(step2, step3)
 
             step4 = make_primitive_module('d3m.primitives.data_transformation.extract_columns_by_semantic_types.DataFrameCommon')
@@ -691,7 +691,7 @@ class GenerateD3MPipelines():
             step7 = make_primitive_module('d3m.primitives.data_transformation.ndarray_to_dataframe.Common')
             connect(step6, step7)
 
-            step8 = make_primitive_module('d3m.primitives.regression.linear_svr.SKlearn')
+            step8 = make_primitive_module('d3m.primitives.regression.random_forest.SKlearn')
             connect(step7, step8)
             connect(step4, step8, to_input='outputs')
 
