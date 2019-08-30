4cdb11327f586fed09d630b426f3ad46296544e8
Roque Lopez <rlopezc27@gmail.com>
2019-08-30 12:59:24 -0400

    Add feature selection to the grammar

---
+++ tests/test_datasets.py
@@ -0,0 +1,133 @@
+import os
+import csv
+import json
+import grpc
+import logging
+import pandas as pd
+import d3m_ta2_nyu.proto.core_pb2_grpc as pb_core_grpc
+from datetime import datetime
+from os.path import dirname, join
+from d3m.metadata.pipeline import Pipeline
+from d3m_ta2_nyu.grpc_logger import LoggingStub
+from d3m_ta2_nyu.common import SCORES_FROM_SCHEMA, SCORES_TO_SKLEARN
+from ta3ta2_api.utils import encode_pipeline_description, ValueType
+from client import do_search, do_train, do_test, do_export
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
+
+
+D3MINPUTDIR = os.environ.get('D3MINPUTDIR')
+D3MOUTPUTDIR = os.environ.get('D3MOUTPUTDIR')
+
+
+def run_all_datasets():
+    channel = grpc.insecure_channel('localhost:45042')
+    core = LoggingStub(pb_core_grpc.CoreStub(channel), logger)
+    statistics_path = join(dirname(__file__), '../resource/statistics_datasets.csv')
+    datasets = sorted([x for x in os.listdir(D3MINPUTDIR) if os.path.isdir(join(D3MINPUTDIR, x))])
+    datasets = ['185_baseball']
+    size = len(datasets)
+    use_template = False
+    pipeline_template = None
+
+    if use_template:
+        pipeline_template = load_template()
+
+    for i, dataset in enumerate(datasets):
+        logger.info('Processing dataset "%s" (%d/%d)' % (dataset, i+1, size))
+        start_time = datetime.now()
+
+        train_dataset_path = '/input/%s/TRAIN/dataset_TRAIN/datasetDoc.json' % dataset
+        test_dataset_path = '/input/%s/TEST/dataset_TEST/datasetDoc.json' % dataset
+        problem_path = join(D3MINPUTDIR, dataset, 'TRAIN/problem_TRAIN/problemDoc.json')
+
+        if not os.path.isfile(problem_path):
+            logger.error('Problem file (%s) doesnt exist', problem_path)
+            continue
+
+        with open(problem_path) as fin:
+            problem = json.load(fin)
+
+        try:
+            metric = SCORES_FROM_SCHEMA[problem['inputs']['performanceMetrics'][0]['metric']]
+        except KeyError as e:
+            logger.error('Metric %s doesnt exist', e)
+
+        task = get_task(problem)
+        best_time, score = 'None', 'None'
+        solutions = do_search(core, problem, train_dataset_path, time_bound=30.0, pipelines_limit=0,
+                              pipeline_template=pipeline_template)
+        search_time = str(datetime.now() - start_time)
+        number_solutions = len(solutions)
+
+        if number_solutions > 0:
+            best_time = sorted(solutions.values(), key=lambda x: x[2])[0][2]
+            best_solution = sorted(solutions.items(), key=lambda x: x[1][0])[-1][0]
+            logger.info('Best pipeline: solution_id=%s' % best_solution)
+
+            fitted_solution = do_train(core, [best_solution], train_dataset_path)
+            tested_solution = do_test(core, fitted_solution, test_dataset_path)
+
+            if len(tested_solution) > 0:
+                true_file_path = join(D3MINPUTDIR, dataset, '%s_dataset/tables/learningData.csv' % dataset)
+                pred_file_path = join(D3MOUTPUTDIR, 'predictions', os.path.basename(list(tested_solution.values())[0]))
+                try:
+                    labels, predictions = get_ytrue_ypred(true_file_path, pred_file_path)
+                    score = calculate_performance(metric, labels, predictions)
+                    logger.info('Best pipeline scored: %s=%.2f' % (metric, score))
+                except Exception as e:
+                    logger.error('Error calculating test score')
+                    logger.error(e)
+
+            #do_export(core, fitted_solution)
+
+        row = [dataset, number_solutions, best_time, search_time, score, metric, task]
+        save_row(statistics_path, row)
+
+
+def get_ytrue_ypred(true_file_path, pred_file_path):
+    labels = pd.read_csv(true_file_path)
+    predictions = pd.read_csv(pred_file_path)
+    col_index, col_label = list(predictions.columns)
+    labels = labels[[col_index, col_label]]
+    labels = pd.merge(labels, predictions[[col_index]], on=[col_index], how='inner')
+
+    return labels[col_label].values, predictions[col_label].values
+
+
+def get_task(problem):
+    task_type = problem['about']['taskType'].upper()
+    if 'taskSubType' in problem['about']:
+        task_type = task_type + '_' + problem['about']['taskSubType'].upper()
+
+    return task_type
+
+
+def calculate_performance(metric, labels, predictions):
+    score = SCORES_TO_SKLEARN[metric](labels, predictions)
+
+    return score.mean()
+
+
+def save_row(file_path, row):
+    with open(file_path, 'a') as fout:
+        writer = csv.writer(fout, delimiter='\t')
+        writer.writerow(row)
+
+
+def load_template():
+    with open(os.path.join(os.path.dirname(__file__), '../resource/pipelines/temporal.json')) as fin:
+        json_pipeline = json.load(fin)
+
+    d3m_pipeline = Pipeline.from_json_structure(json_pipeline, )
+    grpc_pipeline = encode_pipeline_description(d3m_pipeline, [ValueType.RAW], '/tmp')
+
+    return grpc_pipeline
+
+
+if __name__ == '__main__':
+    run_all_datasets()
+
+
---
+++ tests/test_runner.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker exec -it ta2_container python3 tests/test_datasets.py
