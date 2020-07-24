f2afcea64eed52dd571820fd6e78f16d09cc9073
Raoni Lourenco <raoni@nyu.edu>
2020-07-24 16:39:21 -0400

    updating original instead of best

--- d3m_ta2_nyu/pipeline_tune.py
+++ d3m_ta2_nyu/pipeline_tune.py
@@ -104,8 +104,8 @@ def tune(pipeline_id, metrics, problem, dataset_uri, sample_dataset_uri, do_rank
                 .filter(database.PipelineParameter.name == 'hyperparams')
             if query.first():
                 original_parameters = pickle.loads(query.first().value)
-                best_hyperparameters.update(original_parameters)
-                query.update({database.PipelineParameter.value: pickle.dumps(best_hyperparameters)})
+                original_parameters.update(best_hyperparameters)
+                query.update({database.PipelineParameter.value: pickle.dumps(original_parameters)})
             else:
                 db.add(database.PipelineParameter(
                     pipeline=new_pipeline,
