bd2d3ecd5113ba96409b57bcf05eefba5c9cc21b
Roque Lopez <rlopezc27@gmail.com>
2021-03-10 20:34:06 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 
-with io.open('README.rst', encoding='utf-8') as fp:
+with io.open('README.md', encoding='utf-8') as fp:
     description = fp.read()
 req = [
     'grpcio',
@@ -18,7 +18,7 @@ req = [
     'pandas',
     'smac']
 setup(name='d3m_ta2_nyu',
-      version='2020.07.24',
+      version='2020.12.08',
       packages=['d3m_ta2_nyu'],
       package_data={'d3m_ta2_nyu': ['pipelines/*.yaml']},
       entry_points={
@@ -29,10 +29,10 @@ setup(name='d3m_ta2_nyu',
       install_requires=req,
       description="AlphaD3M: NYU's AutoML System",
       long_description=description,
-      author="Remi Rampin, Raoni Lourenco, Roque Lopez",
-      author_email='remi.rampin@nyu.edu, raoni@nyu.edu, rlopez@nyu.edu',
-      maintainer="Remi Rampin, Raoni Lourenco, Roque Lopez",
-      maintainer_email='remi.rampin@nyu.edu, raoni@nyu.edu, rlopez@nyu.edu',
+      author="Remi Rampin, Roque Lopez, Raoni Lourenco",
+      author_email='remi.rampin@nyu.edu, rlopez@nyu.edu, raoni@nyu.edu',
+      maintainer="Remi Rampin, Roque Lopez, Raoni Lourenco",
+      maintainer_email='remi.rampin@nyu.edu, rlopez@nyu.edu, raoni@nyu.edu',
       keywords=['datadrivendiscovery', 'automl', 'd3m', 'ta2', 'nyu'],
       license='Apache-2.0',
       classifiers=[
