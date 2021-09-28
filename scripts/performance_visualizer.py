c70c60d3b9ee5cd0e5744e792e3a45b40c3c0683
Roque Lopez <rlopezc27@gmail.com>
2021-09-28 16:18:23 -0400

    Refactor

--- scripts/performance_visualizer.py
+++ scripts/performance_visualizer.py
@@ -2,14 +2,14 @@ import json
 import altair as alt
 
 
-def load_search_performances(dataset, file_path, mode):
-    search_results = {'mode': [], 'score': [], 'time': []}
+def load_search_performances(dataset, file_path, method):
+    search_results = {'method': [], 'score': [], 'time': []}
 
     with open(file_path) as fin:
         search_data = json.load(fin)
 
     for pipeline in search_data[dataset]['all_scores']:
-        search_results['mode'].append(mode)
+        search_results['method'].append(method)
         search_results['score'].append(pipeline['score'])
         search_results['time'].append(pipeline['time'])
 
@@ -17,8 +17,8 @@ def load_search_performances(dataset, file_path, mode):
 
 
 def plot_search_performances(data):
-    return alt.Chart(data).mark_line().encode(
+    return alt.Chart(data).mark_line(point=True).encode(
         alt.X('hoursminutes(time):T', title='Time'),
         alt.Y('score:Q', title='Score'),
-        color='mode',
+        color='method',
     ).properties(width=800)
