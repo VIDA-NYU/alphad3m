9d9e321197dfcfb9e860db23d4447fb0f678f2ca
Roque Lopez <rlopezc27@gmail.com>
2022-03-22 22:28:31 -0400

    Update API

--- alphad3m_containers/alphad3m_containers/api.py
+++ alphad3m_containers/alphad3m_containers/api.py
@@ -6,29 +6,33 @@ from d3m_interface import AutoML as BaseAutoML
 
 class DockerAutoML(BaseAutoML):
 
-    def __init__(self, output_folder, grpc_port=None, verbose=False):
+    def __init__(self, output_folder, resource_folder=None, grpc_port=None, verbose=False):
         """Create/instantiate an AutoMLContainer object
 
         :param output_folder: Path to the output directory
+        :param resource_folder: Path to the directory where the resources are stored. This is needed only for some
+            primitives that use pre-trained models, databases, etc.
         :param grpc_port: Port to be used by GRPC
         :param verbose: Whether or not to show all the logs from AutoML systems
         """
 
         automl_id = 'AlphaD3M'
         container_runtime = 'docker'
-        BaseAutoML.__init__(self, output_folder, automl_id, container_runtime, grpc_port, verbose)
+        BaseAutoML.__init__(self, output_folder, automl_id, container_runtime, resource_folder, grpc_port, verbose)
 
 
 class SingularityAutoML(BaseAutoML):
 
-    def __init__(self, output_folder, grpc_port=None, verbose=False):
+    def __init__(self, output_folder, resource_folder=None, grpc_port=None, verbose=False):
         """Create/instantiate an AutoMLContainer object
 
         :param output_folder: Path to the output directory
+        :param resource_folder: Path to the directory where the resources are stored. This is needed only for some
+            primitives that use pre-trained models, databases, etc.
         :param grpc_port: Port to be used by GRPC
         :param verbose: Whether or not to show all the logs from AutoML systems
         """
 
         automl_id = 'AlphaD3M'
         container_runtime = 'singularity'
-        BaseAutoML.__init__(self, output_folder, automl_id, container_runtime, grpc_port, verbose)
+        BaseAutoML.__init__(self, output_folder, automl_id, container_runtime, resource_folder, grpc_port, verbose)
