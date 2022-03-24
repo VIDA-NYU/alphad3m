f20eeb721180b74d337cc5f0c6df183a90210312
Roque Lopez <rlopezc27@gmail.com>
2022-03-24 14:29:27 -0400

    Read dataset from args

--- scripts/test_api.py
+++ scripts/test_api.py
@@ -1,11 +1,19 @@
 from alphad3m import AutoML
+import sys
+
+
+if len(sys.argv) > 1:
+    dataset = sys.argv[1]
+else:
+    dataset = '185_baseball_MIN_METADATA'  # Default dataset
 
 output_path = '/Users/rlopez/D3M/examples/tmp/'
-train_dataset = '/Users/rlopez/D3M/datasets/seed_datasets_current/185_baseball_MIN_METADATA/TRAIN'
-test_dataset = '/Users/rlopez/D3M/datasets/seed_datasets_current/185_baseball_MIN_METADATA/TEST'
-score_dataset = '/Users/rlopez/D3M/datasets/seed_datasets_current/185_baseball_MIN_METADATA/SCORE'
+resource_path = '/Users/rlopez/D3M/static/'
+train_dataset = '/Users/rlopez/D3M/datasets/seed_datasets_current/%s/TRAIN' % dataset
+test_dataset = '/Users/rlopez/D3M/datasets/seed_datasets_current/%s/TEST' % dataset
+score_dataset = '/Users/rlopez/D3M/datasets/seed_datasets_current/%s/SCORE' % dataset
 
 
-automl = AutoML(output_path, verbose=True)
-automl.search_pipelines(train_dataset, time_bound=1)
+automl = AutoML(output_path, resource_folder=resource_path, verbose=True)
+automl.search_pipelines(train_dataset, time_bound=5)
 automl.end_session()
